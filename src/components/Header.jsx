df63ef6d23304a6cb09518c7ca0b2dd9781cf62b
Xedinsey <gameeter@gmail.om>
2021-05-19 17:53:50 +0300

    14 lesson

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return <header className={classes.header}>
+        <img src='https://w7.pngwing.com/pngs/530/829/png-transparent-drawing-marker-pen-circle-circle-blue-company-text.png' alt="лого"/>
+    </header>
+}
+
+export default Header;
