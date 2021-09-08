de8afd7b72c0f0aa0c68d45d9118b02dd271aa1a
Xedinsey <gameeter@gmail.om>
2021-09-08 22:32:46 +0300

    44 lesson

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import {addMessageActionCreator, updateNewMessageActionCreator} from "../../Redux/dialogsReducer";
 import Dialogs from "./Dialogs";
+import StoreContext from "../../StoreContext";
 
 
-const DialogsContainer = (props) => {
-    let state = props.store.getState().dialogsPage;
+const DialogsContainer = () => {
 
-    let onSendMessageClick = () => {
-        props.store.dispatch(addMessageActionCreator());
-    };
 
-    let onNewMessageChange = (text) => {
-        props.store.dispatch(updateNewMessageActionCreator(text))
-    }
+    return <StoreContext.Consumer>
+        {
+            (store) => {
 
-    return <Dialogs updateNewMessageBody={onNewMessageChange}
-                    sendMessage={onSendMessageClick}
-                    dialogsPage={state}
-    />
+                let state = store.getState().dialogsPage;
+
+                let onSendMessageClick = () => {
+                    store.dispatch(addMessageActionCreator());
+                };
+
+                let onNewMessageChange = (text) => {
+                    store.dispatch(updateNewMessageActionCreator(text))
+                };
+
+                return <Dialogs updateNewMessageBody={onNewMessageChange}
+                                sendMessage={onSendMessageClick}
+                                dialogsPage={state}
+                />
+            }
+
+        }
+    </StoreContext.Consumer>
 }
 
 export default DialogsContainer;
