2f0fd5b24f4e2b33f6fb189281a2601d61c257a3
Xedinsey <gameeter@gmail.com>
2022-04-20 15:44:37 +0300

    55 lesson. Pagination

--- src/Redux/usersReducer.js
+++ src/Redux/usersReducer.js
@@ -1,46 +1,56 @@
-const FOLLOW = 'FOLLOW';
-const UNFOLLOW = 'UNFOLLOW';
-const SET_USERS = 'SET_USERS';
+const FOLLOW = 'FOLLOW'
+const UNFOLLOW = 'UNFOLLOW'
+const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
 
 
 let initialState = {
     users: [],
-};
+    pageSize: 10,
+    totalUsersCount: 100,
+    currentPage: 1
+}
 
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
         case FOLLOW:
             return {
                 ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userID) {
-                        return {...u, followed: true}
+                users: state.users.map(user => {
+                    if (user.id === action.userID) {
+                        return {...user, followed: true}
                     }
-                    return u;
+                    return user
                 })
             }
         case UNFOLLOW:
             return {
                 ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userID) {
-                        return {...u, followed: false}
+                users: state.users.map(user => {
+                    if (user.id === action.userID) {
+                        return {...user, followed: false}
                     }
-                    return u;
+                    return user
                 })
             }
         case SET_USERS: {
             return {
-                ...state,  users: [...state.users, ...action.users]
+                ...state,  users: action.users
+            }
+        }
+        case SET_CURRENT_PAGE: {
+            return {
+                ...state,  currentPage: action.currentPage
             }
         }
         default:
-            return state;
+            return state
     }
-};
+}
 
 export const followAC = (userID) => ({type: FOLLOW, userID})
 export const unfollowAC = (userID) => ({type: UNFOLLOW, userID})
 export const setUsersAC = (users) => ({type: SET_USERS, users})
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
 
-export default usersReducer;
+export default usersReducer
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,33 +2,44 @@ import React from 'react';
 import * as axios from "axios";
 import userPhoto from '../../assets/images/user_image_mock.png';
 import {Card, Button, Container, Row, Col} from 'react-bootstrap';
+import styles from './Users.module.css';
 
 class Users extends React.Component {
 
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
             this.props.setUsers(response.data.items);
         });
     }
 
-//  Функция запроса пользователей на сервер
-    getUsers = () => {
-
+    onPageChanged  = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
     }
 
     render() {
-        return <>
-            <Button onClick={this.getUsers} style={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                display: this.props.users.length === 0 ? 'block' : 'none'
-            }}>Get Users</Button>
 
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+
+        let pages = []
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
+        return <>
+            <div>
+                {pages.map(page => {
+                    return <Button className={this.props.currentPage === page && styles.selectedPage} onClick={(e) => {
+                        this.onPageChanged(page)
+                    }}>{page}</Button>
+                })}
+            </div>
             <Container style={{margin: '10px', padding: '5px', maxWidth: '800px', minWidth: '300px'}}>
                 <Row md={2} xs={1}>
                     {
-                        this.props.users.map(u =>
+                        this.props.users.map(user =>
 
                             <Col>
                                 <Card style={{width: '18rem', margin: '1rem'}}>
@@ -37,19 +48,19 @@ class Users extends React.Component {
                                         alignSelf: "center",
                                         borderRadius: 100,
                                         paddingTop: '10px'
-                                    }} src={u.photos.small != null ? u.photos.small : userPhoto}/>
+                                    }} src={user.photos.small != null ? user.photos.small : userPhoto}/>
                                     <Card.Body>
-                                        <Card.Title style={{textAlign: "center"}}>{u.name}</Card.Title>
-                                        <Card className="Text">{u.status}</Card>
+                                        <Card.Title style={{textAlign: "center"}}>{user.name}</Card.Title>
+                                        <Card className="Text">{user.status}</Card>
                                         <Card className="Text">{"u.location.country"}</Card>
                                         <Card className="Text">{"u.location.city"}</Card>
                                         <div>
-                                            {u.followed
+                                            {user.followed
                                                 ? <Button variant="primary" onClick={() => {
-                                                    this.props.unfollow(u.id)
+                                                    this.props.unfollow(user.id)
                                                 }}>UnFollow</Button>
                                                 : <Button variant="primary" onClick={() => {
-                                                    this.props.follow(u.id)
+                                                    this.props.follow(user.id)
                                                 }}>Follow</Button>
                                             }
                                         </div>
