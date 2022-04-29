d3f9848e365aaca512f84c85cebdf81a575fcaca
Xedinsey <gameeter@gmail.com>
2022-04-29 20:13:30 +0300

    67 lesson. thunk's more think's

--- src/Redux/profileReducer.js
+++ src/Redux/profileReducer.js
@@ -1,3 +1,5 @@
+import {usersAPI} from "../api/api";
+
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT_ACTION_CREATOR = 'UPDATE-NEW-POST-TEXT-ACTION-CREATOR';
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
@@ -39,5 +41,11 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = () => ({type: ADD_POST});
 export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT_ACTION_CREATOR, newText: text});
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
+
+export const getUserProfile = (userId) => (dispatch) => {
+    usersAPI.getProfile(userId).then(response => {
+        dispatch(setUserProfile(response.data));
+    });
+};
 
 export default profileReducer;
